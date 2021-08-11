594315289fc47312a2dc9350e4f10ca5f5e64102
Gill Chin <gill74123@gmail.com>
2021-08-11 18:23:34 +0800

    add index & design

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,33 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+  var swiper = new Swiper(".swiperContent", {
+    // 循環
+    loop: true,
+
+    // 自動播放
+    // autoplay: true,
+    speed: 700,
+
+    // 看到的slide的數量
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    // 斷點可看見的slide數量
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    },
+    
+    // 分頁
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 });
